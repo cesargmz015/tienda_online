3c17b0f3a1b0375831fa242d8a4263cc86ea1ee6
César Gómez <cesargomez015@gmail.com>
2023-12-19 19:22:11 +0100

    versión definitva v5 (prepared statements)

--- src/php/modelo.php
+++ src/php/modelo.php
@@ -4,8 +4,13 @@ class conexionBBDD {
     function __construct($usuario, $contra, $servidor, $bbdd) {
         $this->conn = new mysqli($servidor, $usuario, $contra, $bbdd);
     }
-    function obtenerDatos($consulta) {
-        return mysqli_query($this->conn, $consulta);
+    function obtenerDatos($consulta, $params = []) {
+        $stmt = $this->conn->prepare($consulta);
+        if ($params) {
+            mysqli_stmt_bind_param($stmt, str_repeat('s', count($params)), ...$params);
+        }
+        mysqli_stmt_execute($stmt);
+        return mysqli_stmt_get_result($stmt);
     }
     function convertirDatos($respuesta) {
         $arrayDatos = array();
@@ -14,11 +19,20 @@ class conexionBBDD {
         }
         return $arrayDatos;
     }
-    function insertarDatos($insercion) {
-        return mysqli_query($this->conn, $insercion);
+    function insertarDatos($insercion, $params = []) {
+        $stmt = $this->conn->prepare($insercion);
+        if ($params) {
+            mysqli_stmt_bind_param($stmt, str_repeat('s', count($params)), ...$params);
+        }
+        mysqli_stmt_execute($stmt);
+        return mysqli_stmt_affected_rows($stmt);
     }
-
-    function eliminarDatos($eliminacion) {
-        return mysqli_query($this->conn, $eliminacion);
+    function eliminarDatos($eliminacion, $params = []) {
+        $stmt = $this->conn->prepare($eliminacion);
+        if ($params) {
+            mysqli_stmt_bind_param($stmt, str_repeat('s', count($params)), ...$params);
+        }
+        mysqli_stmt_execute($stmt);
+        return mysqli_stmt_affected_rows($stmt);
     }
 }
